31a2b70c1c5229d33194c4dad5885d21712604c5
El Mokhtar Ouhrich <eouhrich@e2r11p5.1337.ma>
2024-02-16 18:40:44 +0100

    improving algo

--- client.c
+++ client.c
@@ -1,5 +1,6 @@
-#include <minitalk.h>
+#include "minitalk.h"
 #include <string.h>
+#include <stdio.h>
 int main(int ac, char **av)
 {
 	pid_t	pid;
@@ -8,12 +9,18 @@ int main(int ac, char **av)
 	char	*str;
 
 	if (ac != 3)
+	{
 		return (1);
+	printf("ERROR not enough pars.\n");//////////////
+
+	}
 	pid = atoi(av[1]);///////////////
+	printf("server's PID: %d\n", pid);//////////////
 	len = strlen(av[2]);/////////////
 	if (len == 0)
 		return (1);
-	str = strdup(av[2]);//////////////
+	str = strdup(av[2]);/////////////
+	printf("string to send: %s\n", str);//////////////
 	i = 0;
 	int j = 0;
 	while (i < len)
@@ -22,12 +29,20 @@ int main(int ac, char **av)
 		while(j >= 0)
 		{
 			if ((str[i] >> j & 1) == 0)
+			{
 				kill(pid, SIGUSR1);// BIT 0
+				printf("SIGUSR1 sent\n");
+			}
 			else
+			{
 				kill(pid, SIGUSR2);// BIT 1
+				printf("SIGUSR2 sent\n");
+			}
 			j--;
+			usleep(400);
 		}
 		i++;
 	}
+	
 	return (0);
 }
--- server.c
+++ server.c
@@ -1,23 +1,51 @@
-#include <minitalk.h>
+#include "minitalk.h"
 #include <stdio.h>
 
-void *signal_handler(int sig, siginfo_t *info, void *ucontext)
+void signal_handler(int sig, siginfo_t *info, void *ucontext)
 {
+	static char c;
+	static char counter;
+
 	if (sig == SIGUSR1) // bit 0
-		
+	{
+		c = c | 0; // not necessery
+		// printf("SIGUSR1 RECIVED");//////
+	}
+	else if (sig == SIGUSR2) // bit 1
+	{
+		c = c | 1;
+		// printf("SIGUSR2 RECIVED");//////
+	}
+	counter++;
+	if(counter == 8)
+	{
+		counter = 0;
+		// printf("\ncharacter => %c | its ascci => %d\n", c, c);//////
+		write(1, &c, 1);
+		c = 0;
+	}
+	else
+	{
+		c = c << 1;
+		// printf("==> %d \n", c);//////
+	}
 }
 
 int main()
 {
 	pid_t pid;
-	struct sigaction	sigact;
+	struct sigaction	sigact = { 0 };//////////////////
 
-	sigact.sa_sigaction = signal_handler; 
+	sigact.sa_sigaction = &signal_handler; 
 	sigact.sa_flags = SA_SIGINFO;
-	sigact.sa_mask = 0;
+	// sigact.sa_mask = 0;
 	if (sigaction(SIGUSR1, &sigact, NULL) != 0 || sigaction(SIGUSR2, &sigact, NULL) != 0)
 		return (1);
 	pid = getpid();
 	printf("PID: %d\n", pid);/////////////
+	while(1)
+	{
+		sleep(1);
+	}
 	return(0);
 }
